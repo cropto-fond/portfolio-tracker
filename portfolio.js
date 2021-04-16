b820d2441c132ccde1f7da0c3c6b27e32018d411
David Fanjkutić <dfanjkutic@guidewire.com>
2021-04-16 21:09:51 +0100

    Portfolio - update 0

---
+++ portfolio.js
@@ -0,0 +1,46 @@
+let portfolio = [
+  {
+    "coin": "solana",
+    "units": 4.781913043
+  },
+  {
+    "coin": "syscoin",
+    "units": 313.6261572
+  },
+  {
+    "coin": "trustswap",
+    "units": 18.93512799
+  },
+  {
+    "coin": "polkadot",
+    "units": 3.347283093
+  },
+  {
+    "coin": "icon",
+    "units": 52.43950512
+  },
+  {
+    "coin": "republic-protocol",
+    "units": 133.3822236
+  },
+  {
+    "coin": "tron",
+    "units": 958.128425
+  },
+  {
+    "coin": "pancakeswap-token",
+    "units": 4.541666667
+  },
+  {
+    "coin": "ripple",
+    "units": 102.231068
+  },
+  {
+    "coin": "stellar",
+    "units": 214.5664352
+  },
+  {
+    "coin": "matic-network",
+    "units": 333.9742458
+  }
+];
